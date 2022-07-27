c997d2eedc7804da324bbbe96496a188c6807904
kamisama-shen <9173852qweasdzxc@163.com>
2022-07-27 09:43:28 +0800

    de

--- Example/Pods/Target Support Files/FIPTools/FIPTools-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FIPTools : NSObject
-@end
-@implementation PodsDummy_FIPTools
-@end
--- Example/Pods/Target Support Files/Pods-FIPTools_Example/Pods-FIPTools_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FIPTools_Example : NSObject
-@end
-@implementation PodsDummy_Pods_FIPTools_Example
-@end
